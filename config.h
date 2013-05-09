9c283938e0dac156ad4f1d55acce85efcd74e674
Садовский Николай <sns1081@gmail.com>
2013-05-10 00:17:06 +0700

    Добавил инициализацию логов в тестах

--- config.h
+++ config.h
@@ -8,7 +8,7 @@
 #define MAJOR_VERSION 0
 #define MINOR_VERSION 1
 #define PATCH_VERSION 0
-#define BUILD_DATE "09.05.2013 22:20:55"
+#define BUILD_DATE "10.05.2013 00:02:18"
 
 #define MongoDB_FOUND
 #define RABBITMQ_FOUND
--- test/params.cpp
+++ test/params.cpp
@@ -1,9 +1,14 @@
 #define BOOST_AUTO_TEST_MAIN
 
+#include <log4cplus/logger.h>
+#include <log4cplus/loggingmacros.h>
+#include <log4cplus/configurator.h>
 #include <boost/test/unit_test.hpp>
 
 #include "../config.h"
 #include "../backends/factory.hpp"
+
+using namespace log4cplus;
 
 #ifdef ORACLE_FOUND
 #   include "../backends/oracle.hpp"
@@ -14,6 +19,9 @@
 #ifdef ORACLE_FOUND
 
 BOOST_AUTO_TEST_CASE(test_oracle_backend_construct) {
+
+    BasicConfigurator config;
+    config.configure();
 
     BOOST_REQUIRE_NO_THROW(communicator::backend::oracle_t("login", "password", "10.10.3.23:1234/v3", false));
 
@@ -28,6 +36,9 @@ BOOST_AUTO_TEST_CASE(test_oracle_backend_construct) {
 
 BOOST_AUTO_TEST_CASE(test_oracle_backend_default_port) {
 
+    BasicConfigurator config;
+    config.configure();
+
     BOOST_REQUIRE_NO_THROW(communicator::backend::oracle_t("login", "password", "10.10.3.23:1234/v3", false));
 
     communicator::backend::oracle_t oracle_backend("login", "password", "10.10.3.23/v3", false);
@@ -41,10 +52,16 @@ BOOST_AUTO_TEST_CASE(test_oracle_backend_default_port) {
 
 BOOST_AUTO_TEST_CASE(test_oracle_backend_invalid_params) {
 
+    BasicConfigurator config;
+    config.configure();
+
     BOOST_CHECK_THROW(communicator::backend::oracle_t oracle_backend("login", "password", "huypizdadjigurda", false), std::invalid_argument);
 }
 
 BOOST_AUTO_TEST_CASE(test_oracle_backend_registration) {
+
+    BasicConfigurator config;
+    config.configure();
 
     // BOOST_REQUIRE_NOTHROW(communicator::backend::factory::create("oracle", "login", "password", "huypizdadjigurda"));
 }
