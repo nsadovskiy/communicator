511b66406786b69cb1c4c041e701b74fa2ef9fe1
sns <sns1081@gmail.com>
2013-06-11 11:40:36 +0400

    Started on Window$ with Oracle backend

--- config.h
+++ config.h
@@ -8,10 +8,10 @@
 #define MAJOR_VERSION 0
 #define MINOR_VERSION 1
 #define PATCH_VERSION 0
-#define BUILD_DATE "17.05.2013 09:25:50"
+#define BUILD_DATE "10.06.2013"
 
-#define MongoDB_FOUND
-#define RABBITMQ_FOUND
+/* #undef MongoDB_FOUND */
+/* #undef RABBITMQ_FOUND */
 #define ORACLE_FOUND
 
 #endif // CONFIG_HPP
---
+++ main_win.cpp
@@ -0,0 +1,104 @@
+/**
+ *
+ *
+ **/
+#include <string>
+#include <iostream>
+
+#include <boost/regex.hpp>
+#include <boost/lexical_cast.hpp>
+
+#include <log4cplus/logger.h>
+#include <log4cplus/configurator.h>
+#include <log4cplus/loggingmacros.h>
+
+#include "config.h"
+#include "server.hpp"
+#include "omnicomm/protocol.hpp"
+
+using std::cerr;
+using std::string;
+
+const log4cplus::Logger logger = log4cplus::Logger::getInstance("main");
+
+/**
+ *
+ *
+ **/
+bool fill_settings(int argc, const char * argv[], communicator::settings_t & settings) {
+
+    boost::cmatch match;
+    // boost::regex re("(\\w+)://((\\w+):(\\w+)@)?(([^/:]+)(:(\\d+))?/)?(.+)?");
+    boost::regex re("(\\w+)://((\\w+):(\\w+)@)?(.+)?");
+
+    if (argc < 6 || !boost::regex_match(argv[5], match, re)) {
+        return false;
+    }
+
+    // for (auto m: match) {
+    //     cerr << "Match: " << m << "\n";
+    // }
+
+    string mode = argv[1];
+    if (mode != "daemon" && mode != "nodaemon") {
+        return false;
+    }
+
+    settings.daemon_mode = (string(argv[1]) == "daemon");
+    settings.num_workers = boost::lexical_cast<size_t>(argv[4]);
+    settings.listen.ip_addr = argv[2];
+    settings.listen.port = argv[3];
+    settings.store.protocol = match[1];
+    settings.store.username = match[3];
+    settings.store.password = match[4];
+    settings.store.path = match[5];
+
+    return true;
+}
+
+/**
+ *
+ *
+ **/
+int main(int argc, const char * argv[]) {
+
+	log4cplus::PropertyConfigurator::doConfigure("log4cplus.config");
+
+	try {
+		LOG4CPLUS_INFO(logger, "Communicator v" << MAJOR_VERSION << "." << MINOR_VERSION << "." << PATCH_VERSION << ". Build " << BUILD_DATE << ". (C) VistGroup.");
+
+#       if defined(BOOST_ASIO_HAS_EPOLL)
+            LOG4CPLUS_INFO(logger, "Compiled for Linux, use epoll");
+#       elif defined(BOOST_ASIO_HAS_IOCP)
+            LOG4CPLUS_INFO(logger, "Compiled for frigging windows, use IoCompletionPort");
+#       elif defined(BOOST_ASIO_HAS_KQUEUE)
+            LOG4CPLUS_INFO(logger, "Compiled for FreeBSD or faggot Mac Os X, use kqueue");
+#       elif defined(BOOST_ASIO_HAS_DEV_POLL)
+            LOG4CPLUS_INFO(logger, "Compiled for Solaris, use /dev/poll");
+#       else
+            LOG4CPLUS_INFO(logger, "Where am I ?!? O_O");
+#       endif
+
+		communicator::settings_t settings;
+
+        if (!fill_settings(argc, argv, settings)) {
+            cerr << "Usage: communicator <mode> <ip-address> <port> <threads> <store>\n";
+            cerr << "  <daemon|nodaemon>  - run mode console or daemon\n";
+            cerr << "  <ip-address>       - listened address to bind\n";
+            cerr << "  <port>             - listened port\n";
+            cerr << "  <threads>          - number woring threads\n";
+            cerr << "  <store>            - storage connection protocol://login:password@path\n";
+            cerr << "\n";
+            cerr << "Example:\n";
+            cerr << "  communicator nodaemon 0.0.0.0 4010 4 oracle://login:password@db-server/database_sid\n";
+            exit(1);
+        }
+
+		omnicomm::transport_protocol_t::init_protocol();
+		communicator::server_t server(settings, omnicomm::transport_protocol_t::create);
+        server.run();
+
+	} catch (const std::exception & e) {
+        LOG4CPLUS_FATAL(logger, e.what());
+    }
+}
